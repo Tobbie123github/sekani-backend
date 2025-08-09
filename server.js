ff6f90587c3f795a93ccd152217c96c43babccc6
Tobbie123github <tobbie41973@gmail.com>
2025-08-10 00:03:48 +0100

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sekani-frontend.vercel.app/",
     credentials: true, // allow frontend to send cookies
   })
 );
